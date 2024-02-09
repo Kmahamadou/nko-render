da51b5a0e4219adf2cfff624f73f12a468fda927
Kmahamadou <kmahamadou01@gmail.com>
2024-02-09 16:18:22 +0000

    Enabled visiter email sending

--- app/Http/Controllers/contactController.php
+++ app/Http/Controllers/contactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 use App\Jobs\ProcessVisiterEmail;
+use App\Mail\VisiterEmail;
 
 
 class contactController extends Controller
