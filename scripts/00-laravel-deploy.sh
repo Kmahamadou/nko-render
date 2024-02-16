a695d21dfeab97aacc3300cde882fd8c3376d9cb
Kmahamadou <kmahamadou01@gmail.com>
2024-02-16 15:06:12 +0000

    Initial commit

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -9,5 +9,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Caching routes..."
+php artisan view:cache
+
 echo "Running migrations..."
-php artisan migrate:fresh --force
+php artisan migrate --force
