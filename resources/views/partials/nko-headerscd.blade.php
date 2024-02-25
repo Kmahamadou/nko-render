72590083237f25b5e3cca86ae1aee0364e754912
Kmahamadou <kmahamadou01@gmail.com>
2024-02-25 12:18:09 +0000

    Fixed issues arose from adding first NKO version

---
+++ resources/views/partials/nko-headerscd.blade.php
@@ -0,0 +1,49 @@
+{{-- <header id="header" class="d-flex align-items-center" style="background-color: #F0F0F0; height: 20%;">
+    <div class="container d-flex align-items-center justify-content-between" > --}}
+<div class="sub_menu_wrapper">
+    <div class="sub_menu">
+
+        <div>
+            <a class="nav-link scrollto" href="/nko/audio-nko">
+                <i class="fas fa-microphone" style="margin-right:5px;color: #DC143C;"></i>
+                ߒߞߏ ߝߐ߫
+            </a>
+        </div>
+
+        <span style="margin: 0 1em">|</span>
+
+        <div>
+            <a class="nav-link scrollto " href="/nko/frenchtonko">
+                <i class="fas fa-language" style="margin-right:5px;color: #00CED1;"></i>
+                ߞߊ߬ ߝߊ߬ߙߊ߲߬ߛߌ߬ߞߊ߲ ߘߟߊߡߌ߬ߘߊ߬ ߒߞߏ ߘߐ߫
+            </a>
+        </div>
+
+
+        <span style="margin: 0 1em">|</span>
+
+
+        <div>
+          <a class="nav-link scrollto" href="/nko/nkotext">
+                <i class="bi bi-pencil" style="margin-right:5px; color: #3399FF;"></i>
+                ߞߊ߬ ߒߞߏ ߛߓߍ߫
+           </a>
+        </div>
+
+
+        <span style="margin: 0 1em">|</span>
+
+
+       <div>
+            <a class="nav-link scrollto" href="/nko/frenchtext">
+                <i class="fas fa-keyboard" style="margin-right:5px; color:#FFD700;"></i>
+                ߞߊ߬ ߝߊ߬ߙߊ߲߬ߛߌ߬ߞߊ߲ ߛߓߍ߫
+            </a>
+        </div>
+
+
+
+          <!-- <div><a href="#"><span>Ecrire Français</span> <i class="bi bi-chevron-down"></i></a> -->
+          </div><!-- .navbar -->
+{{-- </div>
+</header> --}}
